c3f92f7e176d3603f2411624563dc41e17601431
Airiell <airiellgarrett@gmail.com>
2023-04-27 00:41:23 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoapStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
